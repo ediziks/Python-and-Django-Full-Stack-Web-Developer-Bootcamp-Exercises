d4851af33ed436783ea3bb0f5721a7ccd1ea0d2a
Ediz Emektas <eemektas@gmail.com>
2020-01-09 01:59:56 +0300

    Update spyProject.js

--- JavaScript_Level_One/spyProject.js
+++ JavaScript_Level_One/spyProject.js
@@ -3,6 +3,7 @@ var ln = prompt("Last name: ")
 var height = prompt("Height: ")
 var age = prompt("Age: ")
 var pet = prompt("Pet name: ")
+alert("Thanks for the info!")
 
 if (fn[0] === ln[0] && height >= 170 && 21 <= age <= 29 && pet.slice(-1) === "y"){
 	console.log("Welcome SPY!")
