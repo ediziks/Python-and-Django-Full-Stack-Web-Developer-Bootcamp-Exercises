85e1ac812219747deb182ae37a24c03ba0ea78b5
eemektas <eemektas@gmail.com>
2020-01-25 20:02:40 +0300

    still loading

--- jQuery/MyScript.js
+++ jQuery/MyScript.js
@@ -10,6 +10,21 @@ $('h2').click(function(){
 	$(this).text('I WAS CLICKED MAN!')
 })
 
+// keypress
 $('input').eq(0).keypress(function(){
 	$('h3').toggleClass('turnBlue')
+})
+
+// on
+$('h1').on('dblclick', function(){
+	$(this).toggleClass('turnBlue')
+})
+
+$('h2').on('mouseenter', function(){
+	$(this).toggleClass('turnRed')
+})
+
+// page fades out in 1000ms(turns whitepage)
+$('input').eq(1).on('click', function(){
+	$('.container').fadeOut(1000)
 })
