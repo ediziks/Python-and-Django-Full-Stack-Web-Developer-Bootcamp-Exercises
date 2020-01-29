9ccbf46ffcc54e98c052be6e14a69ac45e988a5c
eemektas <eemektas@gmail.com>
2020-01-29 13:39:27 +0300

    still loading

--- jQuery/FrontEndProject.js
+++ jQuery/FrontEndProject.js
@@ -1,5 +1,5 @@
 var player1 = prompt("Player One: Enter Your Name , you will be Blue");
-var player1Color = 'rgb(86, 151, 255)';
+var player1Color = 'blue';
 
 var player2 = prompt("Player Two: Enter Your Name, you will be Red");
 var player2Color = 'rgb(237, 45, 73)';
