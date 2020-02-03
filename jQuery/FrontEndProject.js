0f325299ab5ba77d093858bf30f995c52516f7be
eemektas <eemektas@gmail.com>
2020-02-03 13:12:56 +0300

    minor change

--- jQuery/FrontEndProject.js
+++ jQuery/FrontEndProject.js
@@ -1,5 +1,5 @@
 var player1 = prompt("Player One: Enter Your Name , you will be Blue");
-var player1Color = 'blue';
+var player1Color = 'rgb(86, 151, 255)';
 
 var player2 = prompt("Player Two: Enter Your Name, you will be Red");
 var player2Color = 'rgb(237, 45, 73)';
@@ -90,6 +90,7 @@ function gameEnd(winningPlayer) {
   }
 }
 
+//Start with Player 1
 var currentPlayer = 1;
 var currentName = player1;
 var currentColor = player1Color;
