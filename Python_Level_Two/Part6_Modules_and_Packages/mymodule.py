99b995d44d19cd25a8d66fd970e71df8d39c5ea0
eemektas <eemektas@gmail.com>
2020-02-20 13:18:46 +0300

    e.g.s extended

--- Python_Level_Two/Part6_Modules_and_Packages/mymodule.py
+++ Python_Level_Two/Part6_Modules_and_Packages/mymodule.py
@@ -3,3 +3,9 @@
 
 def func_in_mymodule():
     print("I am a function inside of the mymodule.py file!")
+    print('Also, much more classes and defs can be added in here!')
+
+def another_func_in_mymodule():
+	print('As you can see, another def added below')
+	print('And you can see it trough another file just by a call')
+	print("It's like magic! Huh?")
