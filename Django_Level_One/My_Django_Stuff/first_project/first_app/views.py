a6698ea47f9fe0d0fc1c26ef41b1c59fe721666d
eemektas <eemektas@gmail.com>
2020-04-06 15:35:41 +0300

    first project

--- Django_Level_One/My_Django_Stuff/first_project/first_app/views.py
+++ Django_Level_One/My_Django_Stuff/first_project/first_app/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(req):
-  return HttpResponse('Hello World!')
-  
+  # return HttpResponse('Hello World!') // this is before templates
+  my_dict = {'insert_me': 'Coming from first_app/index.html'}
+  return render(req, 'first_app/index.html', context=my_dict)
