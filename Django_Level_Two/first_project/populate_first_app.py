8aef5265589a0d529faa1aba4c606a4fcb30a23e
eemektas <eemektas@gmail.com>
2020-04-14 20:57:45 +0300

    django populate

--- Django_Level_Two/first_project/populate_first_app.py
+++ Django_Level_Two/first_project/populate_first_app.py
@@ -1,16 +1,18 @@
 import os
+# Configure settings for project
+# Need to run this before calling models from application!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','first_project.settings')
+
 import django
-import random
-from first_app.models import AccessRecord, Topic, Webpage
-from faker import Faker
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
-
+# Import settings
 django.setup()
 
+import random
+from first_app.models import Topic,Webpage,AccessRecord
+from faker import Faker
+
 fakegen = Faker()
-topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+topics = ['Search','Social','Marketplace','News','Games']
 
 def add_topic():
   t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
@@ -19,14 +21,18 @@ def add_topic():
 
 def populate(N=5):
   for entry in range(N):
+    # get the topic for the entry
     top = add_topic()
 
+    # create the fake data for that entry
     fake_url = fakegen.url()
     fake_date = fakegen.date()
     fake_name = fakegen.company()
 
+    # create the new webpage entry
     webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
 
+    # create a fake access record for that webpage
     acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
 
 if __name__ == '__main__':
