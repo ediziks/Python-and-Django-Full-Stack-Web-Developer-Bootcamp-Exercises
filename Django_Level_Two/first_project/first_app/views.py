433bf9a86fd6275ac313119f60a4006d5ac51ce3
eemektas <eemektas@gmail.com>
2020-04-15 16:19:34 +0300

    views

--- Django_Level_Two/first_project/first_app/views.py
+++ Django_Level_Two/first_project/first_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(req):
-  my_dict = {'insert_content': 'Hello from first_app'}
-  return render(req, 'first_app/index.html', context=my_dict)
+  webpages_list = AccessRecord.objects.order_by('date')
+  date_dict = {'access_records': webpages_list}
+  return render(req, 'first_app/index.html', context=date_dict)
