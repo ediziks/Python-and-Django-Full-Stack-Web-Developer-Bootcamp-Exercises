042637a68b4c47e874748f7afac64dd434b49a2b
eemektas <eemektas@gmail.com>
2020-05-07 23:19:56 +0300

    dj lvl5 basic_app

--- Django_Level_Five/learning_users/basic_app/views.py
+++ Django_Level_Five/learning_users/basic_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm, UserProfileForm
 
 # Create your views here.
+def index(req):
+  return render(req, 'basic_app/index.html')
+
+def register(req):
+  registered = False
+
+  if req.method == "POST":
+    user_form = UserForm(data=req.POST)
+    profile_form = UserProfileForm(data=req.POST)
+
+    if user_form.is_valid() and profile_form.is_valid():
+      user = user_form.save()
+      user.set_password(user.password)
+      user.save()
+
+      profile = profile_form.save(commit=False)
+      profile.user = user
+
+      if 'profile_pic' in req.FILES:
+        profile.profile_pic = req.FILES['profile_pic']
+
+      profile.save()
+
+      registered = True
+    else:
+      print(user_form.errors, profile_form.errors)
+  else:
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+
+  return render(req, 'basic_app/registration.html', 
+                     {'user_form': user_form,
+                      'profile_form': profile_form,
+                      'registered': registered})
