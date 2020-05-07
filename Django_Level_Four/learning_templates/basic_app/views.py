bc5a6d0f7d671398c4e02fb56edcbe89e08799d3
eemektas <eemektas@gmail.com>
2020-05-07 21:23:10 +0300

    django level 4 basic_app

--- Django_Level_Four/learning_templates/basic_app/views.py
+++ Django_Level_Four/learning_templates/basic_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(req):
-  return render(req, 'basic_app/index.html')
+  context_dict = {'text':'hello there', 'num':'999'}
+  return render(req, 'basic_app/index.html', context_dict)
 
 def other(req):
   return render(req, 'basic_app/other.html')
