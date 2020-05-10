017ebc303c434d6129a9ca6f29d31f57bfa2787e
eemektas <eemektas@gmail.com>
2020-05-10 10:46:20 +0300

    adv dj cbv rest of it

---
+++ Advanced_Django_CBV/advcbv/cbv_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from cbv_app import views
+
+app_name = 'cbv_app'
+
+urlpatterns = [
+  path('', views.SchoolListView.as_view(), name='list'),
+  re_path(r'^(?P<pk>\d+)/$', views.SchoolDetailView.as_view(), name='detail'),
+  path('create/', views.SchoolCreateView.as_view(), name='create'),
+  # pk is same as re_path 2 above
+  path('update/<int:pk>', views.SchoolUpdateView.as_view(), name='update'),
+  path('delete/<int:pk>', views.SchoolDeleteView.as_view(), name='delete')
+]
--- Advanced_Django_CBV/advcbv/cbv_app/views.py
+++ Advanced_Django_CBV/advcbv/cbv_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView,
+                                  ListView, DetailView,
+                                  CreateView, UpdateView,
+                                  DeleteView)
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 # def index(req):
@@ -13,7 +18,30 @@ from django.http import HttpResponse
 class IndexView(TemplateView):
   template_name = 'index.html'
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['injectme'] = 'I am INJECTED!'
-    return context
+#   def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     context['injectme'] = 'I am INJECTED!'
+#     return context
+
+class SchoolListView(ListView):
+  context_object_name = 'schools'
+  model = models.School
+  # returns 'school_list'
+  # after contex_object_name it returns 'schools'
+
+class SchoolDetailView(DetailView):
+  context_object_name = 'school_detail'
+  model = models.School
+  template_name = 'cbv_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+  fields = ('name', 'principal', 'location')
+  model = models.School
+
+class SchoolUpdateView(UpdateView):
+  fields = ('name', 'principal')
+  model = models.School
+
+class SchoolDeleteView(DeleteView):
+  model = models.School
+  success_url = reverse_lazy('cbv_app:list')
