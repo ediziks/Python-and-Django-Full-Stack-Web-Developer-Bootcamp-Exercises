913ca5934abb7378392804fe1819a76f2443fa41
eemektas <eemektas@gmail.com>
2020-05-25 13:12:42 +0300

    minor

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Project and exercise solutions of Python and Django Full Stack Web Developer Bootcamp studied on https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp/
+
--- Simple_Social_Clone/simplesocial/groups/models.py
+++ Simple_Social_Clone/simplesocial/groups/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+from django import template
+import misaka
+
 
 # Create your models here.
+User = get_user_model()
+
+register = template.library()
+
+
+class Group(models.Model):
+  pass
+
+
+class GroupMember(models.Model):
+	group = models.ForeignKey(Group, related_name='memberships')
+	user = models.ForeignKey(User, related_name='user_group')
+
+  def __str__(self):
+    return self.username
+
+  class Meta:
+    unique_together = ('group', 'user')
