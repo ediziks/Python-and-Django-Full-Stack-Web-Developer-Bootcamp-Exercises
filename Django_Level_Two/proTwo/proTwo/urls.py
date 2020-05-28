7f9c9c95debfadbb5984c3f2cb225ee0556c7490
eemektas <eemektas@gmail.com>
2020-05-28 19:42:39 +0300

    django custom toolbar

--- Django_Level_Two/proTwo/proTwo/urls.py
+++ Django_Level_Two/proTwo/proTwo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
 from appTwo import views
 
 urlpatterns = [
@@ -24,3 +25,10 @@ urlpatterns = [
   path('admin/', admin.site.urls),
   path('users/', views.users, name="users")
 ]
+
+if settings.DEBUG:
+  import debug_toolbar
+
+  urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls))
+  ] + urlpatterns
